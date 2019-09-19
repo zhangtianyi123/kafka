c651abc010cbd2c1e4e1bd5b6ae55bb3c88d350f
zhangty <465355874@qq.com>
2019-09-19 15:37:15 +0800

    base commit

---
+++ kafka/src/main/java/zty/practise/kafka/KafkaApp.java
@@ -0,0 +1,13 @@
+package zty.practise.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaApp.class, args);
+	}
+	
+}
