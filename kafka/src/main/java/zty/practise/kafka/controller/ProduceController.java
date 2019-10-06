23090fadac4f084810fc5b6c96031b9e589fc09c
zhangty <465355874@qq.com>
2019-10-06 16:57:52 +0800

    实体类生产者

--- kafka/src/main/java/zty/practise/kafka/controller/ProduceController.java
+++ kafka/src/main/java/zty/practise/kafka/controller/ProduceController.java
@@ -1,10 +1,15 @@
 package zty.practise.kafka.controller;
 
+import java.time.LocalDateTime;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import zty.practise.kafka.model.RequestEntity;
 
 @RestController
 public class ProduceController {
@@ -12,9 +17,23 @@ public class ProduceController {
 	@Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
 	
+	private static long count = 0;
+	
 	@GetMapping("/message/send")
     public String send(@RequestParam String message){
         kafkaTemplate.send("topicA",message);
         return "send success: " + message;
+    }
+	
+	@GetMapping("/send")
+    public String requestSend(){
+		RequestEntity entity = new RequestEntity();
+		entity.setEventName(LocalDateTime.now().toString());
+		entity.setLotName(RandomStringUtils.randomNumeric(5));
+		entity.setProcName(RandomStringUtils.randomAscii(5));
+		entity.setReqId((count++) + "");
+		
+        kafkaTemplate.send("topicA", entity);
+        return "send success: " + entity;
     }
 }
