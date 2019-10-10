d07980cf652f3ea3e4cac62c22aa332b045f404b
zhangty <465355874@qq.com>
2019-10-10 19:57:11 +0800

    表表左连接

--- kafka/src/main/java/zty/practise/kafka/stream/join/TableTableLeftJoinApp.java
+++ kafka/src/main/java/zty/practise/kafka/stream/join/TableTableLeftJoinApp.java
@@ -1,26 +1,21 @@
 package zty.practise.kafka.stream.join;
 
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.JoinWindows;
-import org.apache.kafka.streams.kstream.Joined;
-import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.kstream.Serialized;
 
-public class TableTableInnerJoinApp {
+public class TableTableLeftJoinApp {
 
 	public static void main(String[] args) {
 		Properties props = new Properties();
-		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "tabletableinnerjoin_app_id");
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "tabletableleftjoin_app_id");
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.192.202:9092");
 		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
@@ -30,7 +25,7 @@ public class TableTableInnerJoinApp {
 		KTable<String, Long> leftTable = builder.table("topicLong", Consumed.with(Serdes.String(), Serdes.Long()));
 		KTable<String, Double> rightTable = builder.table("topicDouble", Consumed.with(Serdes.String(), Serdes.Double()));
 		
-		KTable<String, String> joinedTable = leftTable.join(rightTable,
+		KTable<String, String> joinedTable = leftTable.leftJoin(rightTable,
 			    (leftValue, rightValue) -> "left=" + leftValue + ", right=" + rightValue, 
 			    Materialized.with(Serdes.String(), Serdes.String()));
 		
